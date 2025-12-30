cf310eeb7f35c923a8ad90fd2dd7a241d26b0912
ArthurFernandes06 <artrdgs@gmail.com>
2025-12-30 13:03:12 -0300

    Criando o database.py

---
+++ core/database.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+#Aqui estamos definindo o banco sqlite que vamos usar.
+DATABASE_URL = "sqlite///./app.db"
+
+
+#Estamos conectando ao banco de dados, se ele não tiver sido criado, ele cria.
+#O parametro echo=True é para aprendizado e visualizar as alterações realizadas,
+#em projetos reais ele é False
+engine = create_engine(DATABASE_URL, echo=True)
+
+#Estabelece uma conexão segura com o banco de dados para executar os comandos SQL.
+session_local = sessionmaker(bind=engine)
+
+#É a base que nossos models vão herdar para a orm mapear.
+Base = declarative_base()
